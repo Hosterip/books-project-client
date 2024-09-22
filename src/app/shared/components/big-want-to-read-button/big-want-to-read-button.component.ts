cf62028d43193ec228f3c0285c9cad38810c9423
Hosterip <hosteripwork@gmail.com>
2024-09-22 15:35:48 +0200

    content for modal

--- src/app/shared/components/big-want-to-read-button/big-want-to-read-button.component.ts
+++ src/app/shared/components/big-want-to-read-button/big-want-to-read-button.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {ModalComponent} from "../modal/modal.component";
+import {ButtonComponent} from "../button/button.component";
 
 @Component({
   selector: 'app-big-want-to-read-button',
   standalone: true,
   imports: [
-    ModalComponent
+    ModalComponent,
+    ButtonComponent
   ],
   templateUrl: './big-want-to-read-button.component.html',
   styleUrl: './big-want-to-read-button.component.scss'
 })
 export class BigWantToReadButtonComponent {
   public isOpen:boolean = true;
+
+  toggleIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
