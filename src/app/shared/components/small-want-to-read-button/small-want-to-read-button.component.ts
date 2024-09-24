2e618d49b7361bdb14ba72286b7d5469facade03
Hosterip <hosteripwork@gmail.com>
2024-09-24 23:28:20 +0200

    small-want-to-read-button finished

--- src/app/shared/components/small-want-to-read-button/small-want-to-read-button.component.ts
+++ src/app/shared/components/small-want-to-read-button/small-want-to-read-button.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {BookshelfService} from "../../../core/services/bookshelf.service";
+import {DefaultBookshelfNames} from "../../enums/DefaultBookshelfNames";
+import {IPaginated} from "../../interfaces/IPaginated";
+import {IBook} from "../../interfaces/IBook";
 
 @Component({
   selector: 'app-small-want-to-read-button',
@@ -11,5 +15,27 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './small-want-to-read-button.component.scss',
 })
 export class SmallWantToReadButtonComponent {
+  @Input({required: true}) bookId: string = ''
+  isOpen:boolean = false;
 
+  constructor(private bookshelfService: BookshelfService) {  }
+
+  toggleIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+  addToBookshelf(bookshelf: DefaultBookshelfNames) : void {
+    const observerOrNext = {
+      next: (res: any) => {
+        console.log(res)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    }
+    this.bookshelfService.AddBookToDefaultBookshelf(bookshelf, this.bookId)
+      .subscribe(observerOrNext)
+  }
+
+  protected readonly DefaultBookshelfNames = DefaultBookshelfNames;
 }
