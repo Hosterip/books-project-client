2c0225a42ef3e7e0f91456b43270241f0c28632c
Hosterip <hosteripwork@gmail.com>
2024-09-25 16:59:01 +0200

    big want to read button component is finished

--- src/app/shared/components/big-want-to-read-button/big-want-to-read-button.component.ts
+++ src/app/shared/components/big-want-to-read-button/big-want-to-read-button.component.ts
@@ -1,21 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ModalComponent} from "../modal/modal.component";
 import {ButtonComponent} from "../button/button.component";
+import {NgOptimizedImage} from "@angular/common";
+import {BookshelfService} from "../../../core/services/bookshelf.service";
+import {DefaultBookshelfNames} from "../../enums/DefaultBookshelfNames";
 
 @Component({
   selector: 'app-big-want-to-read-button',
   standalone: true,
-  imports: [
-    ModalComponent,
-    ButtonComponent
-  ],
+    imports: [
+        ModalComponent,
+        ButtonComponent,
+        NgOptimizedImage
+    ],
   templateUrl: './big-want-to-read-button.component.html',
   styleUrl: './big-want-to-read-button.component.scss'
 })
 export class BigWantToReadButtonComponent {
+  @Input({required: true}) bookId: string = ''
   public isOpen:boolean = true;
 
   toggleIsOpen() {
     this.isOpen = !this.isOpen;
   }
+  constructor(private bookshelfService: BookshelfService) {  }
+
+  addToBookshelf(bookshelf: DefaultBookshelfNames) : void {
+    if(this.bookId == '') return;
+    const observerOrNext = {
+      next: (res: any) => {
+        console.log(res)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    }
+    this.bookshelfService.AddBookToDefaultBookshelf(bookshelf, this.bookId)
+      .subscribe(observerOrNext)
+  }
+
+  protected readonly DefaultBookshelfNames = DefaultBookshelfNames;
 }
