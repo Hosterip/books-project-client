a16827f40f33371d1c1606051837d8dc44c77d52
Hosterip <hosteripwork@gmail.com>
2024-10-10 16:50:22 +0200

    reviews

---
+++ src/app/core/services/reviews.service.ts
@@ -0,0 +1,86 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {getDefaultOptions} from "../../shared/API/getDefaultOptions";
+import {IGenre} from "../../shared/interfaces/IGenre";
+import {catchError} from "rxjs";
+import {handleHttpError} from "../../shared/utils/handleHttpError";
+import {IPaginated} from "../../shared/interfaces/IPaginated";
+import {IReview} from "../../shared/interfaces/IReview";
+import {IBook} from "../../shared/interfaces/IBook";
+import {reviewEndpoints} from "../../shared/API/Endpoints/reviewEndpoints";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ReviewsService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getMany(
+        bookId: number,
+        page: number = 1,
+        limit: number = 10,
+        query?: string | null,
+    ) {
+        const options = getDefaultOptions();
+
+        const params = new HttpParams()
+        params.appendAll({
+            page: page,
+            limit: limit,
+        })
+        if (query)
+            params.append("query", query)
+        return this.http.get<IPaginated<IReview>>(reviewEndpoints.getMany + bookId, {
+            ...options,
+            params
+        })
+            .pipe(catchError(handleHttpError))
+    }
+
+    create(
+        bookId: number,
+        body: string,
+        rating: string
+    ) {
+        const options = getDefaultOptions();
+        const reqBody = {
+            bookId,
+            body,
+            rating,
+        }
+
+        return this.http.post<IReview>(reviewEndpoints.create, reqBody, {
+            ...options,
+        })
+            .pipe(catchError(handleHttpError))
+    }
+
+    update(
+        reviewId: number,
+        body: string,
+        rating: number
+    ) {
+        const options = getDefaultOptions();
+        const reqBody = {
+            reviewId,
+            body,
+            rating,
+        }
+
+        return this.http.post<IReview>(reviewEndpoints.update, reqBody, {
+            ...options,
+        })
+            .pipe(catchError(handleHttpError))
+    }
+
+    delete() {
+        const options = getDefaultOptions();
+
+        return this.http.delete<string>(reviewEndpoints.delete, {
+            ...options,
+        })
+            .pipe(catchError(handleHttpError))
+    }
+}
---
+++ src/app/shared/interfaces/IReview.ts
@@ -0,0 +1,5 @@
+export interface IReview {
+    id: string,
+    body: string,
+    rating: number
+}
