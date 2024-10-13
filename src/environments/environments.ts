603617a1dbc6519c5641c1d97e6b8f22c220c849
Hosterip <hosteripwork@gmail.com>
2024-10-13 18:03:12 +0200

    new default api endpoint

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 import {IEnvironments} from "./IEnvironments";
 
 export const environment: IEnvironments = {
-  apiUrl: 'http://localhost:7190',
-  imageUrl: 'http://localhost:7190/images/',
+  apiUrl: 'http://localhost:8080',
+  imageUrl: 'http://localhost:8080/images/',
 };
